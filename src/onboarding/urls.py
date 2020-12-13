28f8e5a53b9029d55d415ba18dec54037152f7db
Andrey Volkov <andrew.wolf.alex@gmail.com>
2020-12-13 14:59:52 +0300

    Fix API urls

--- src/onboarding/urls.py
+++ src/onboarding/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework import routers
 from rest_framework.authtoken import views
@@ -9,9 +9,11 @@ from .views import ChecklistViewSet
 router = routers.SimpleRouter()
 router.register('checklists', ChecklistViewSet)
 
+api_urlpatterns = [
+    path('tokens/', views.obtain_auth_token),
+] + router.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/tokens/', views.obtain_auth_token),
+    path('api/', include(api_urlpatterns)),
 ]
-
-urlpatterns += router.urls
