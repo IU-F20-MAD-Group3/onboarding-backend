05022e24847ac8ee083958375115fbd73859b741
Andrey Volkov <andrew.wolf.alex@gmail.com>
2020-12-14 21:23:52 +0300

    Add more task endpoints

--- src/onboarding/views.py
+++ src/onboarding/views.py
@@ -1,7 +1,11 @@
+from http import HTTPStatus
+
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from .models import Checklist, Task
+from .models import Task, TaskExecution
 from .serializers import ChecklistSerializer, TaskSerializer
 from .permissions import IsMember
 
@@ -13,7 +17,7 @@ class ChecklistViewSet(
 ):
     """A viewset for viewing checklist instances.
 
-    Provides `list` and `retrieve` actions.
+    Provides `list`, `retrieve` and `tasks` actions.
 
     """
     serializer_class = ChecklistSerializer
@@ -26,6 +30,13 @@ class ChecklistViewSet(
         organization = user.member.organization
         return organization.checklists.all()
 
+    @action(detail=True, methods=['get'], serializer_class=TaskSerializer)
+    def tasks(self, request, pk=None):
+        checklist = self.get_object()
+        tasks = checklist.tasks.all()
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
+
 
 class TaskViewSet(
     mixins.RetrieveModelMixin,
@@ -33,7 +44,7 @@ class TaskViewSet(
 ):
     """A viewset for retrieving task instances.
 
-    Provides `retrieve` action.
+    Provides `retrieve` and `finish` actions.
 
     """
     serializer_class = TaskSerializer
@@ -45,3 +56,19 @@ class TaskViewSet(
         user = self.request.user
         organization = user.member.organization
         return Task.objects.filter(checklist__organization=organization)
+
+    @action(detail=True, methods=['post'])
+    def finish(self, request, pk=None):
+        """Updates task's status to finished.
+        """
+        user = self.request.user
+        member = user.member
+        task = self.get_object()
+
+        TaskExecution.objects.update_or_create(
+            member=member,
+            task=task,
+            defaults={'status': TaskExecution.STATUS_FINISHED},
+        )
+
+        return Response(status=HTTPStatus.NO_CONTENT)
