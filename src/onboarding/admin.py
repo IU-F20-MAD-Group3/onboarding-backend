121d97678cc2f2b1ad3bf12bb304eb02e1961512
Andrey Volkov <andrew.wolf.alex@gmail.com>
2020-12-14 21:35:10 +0300

    Exclude unused model from admin

--- src/onboarding/admin.py
+++ src/onboarding/admin.py
@@ -5,7 +5,6 @@ from . import models
 admin.site.register(models.Organization)
 admin.site.register(models.Member)
 admin.site.register(models.Checklist)
-admin.site.register(models.ChecklistParticipation)
 admin.site.register(models.Task)
 admin.site.register(models.TaskExecution)
 admin.site.register(models.News)
