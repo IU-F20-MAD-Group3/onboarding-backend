866b260d044abda97fac591fb2419de4186157b3
Andrey Volkov <andrew.wolf.alex@gmail.com>
2020-12-14 18:13:13 +0300

    Add models to admin

--- src/onboarding/admin.py
+++ src/onboarding/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Organization, Checklist
+from . import models
 
-admin.site.register(Organization)
-admin.site.register(Checklist)
+admin.site.register(models.Organization)
+admin.site.register(models.Member)
+admin.site.register(models.Checklist)
+admin.site.register(models.ChecklistParticipation)
+admin.site.register(models.Task)
+admin.site.register(models.TaskExecution)
+admin.site.register(models.News)
